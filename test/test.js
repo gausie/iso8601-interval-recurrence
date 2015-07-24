ce6d19aa03716a094a6d7f3368c742ef253bd35f
Sam Gaus <sam@gaus.co.uk>
2015-07-24 13:18:56 +0100

    Give examples of the library taking a JavaScript Date as input instead of text (even though both work).

--- test/test.js
+++ test/test.js
@@ -16,8 +16,8 @@ describe('iso8601-interval-recurrence', function () {
 			recurrence: 'R/P1W'
 		});
 
-		var wednesday = '2015-07-29T13:00:00.000Z';
-		var tuesday = '2015-07-23T11:12:13.000Z';
+		var wednesday = new Date('2015-07-29T13:00:00.000Z');
+		var tuesday = new Date('2015-07-23T11:12:13.000Z');
 
 		assert.equal(every_wednesday.containsDate(wednesday), true);
 		assert.equal(every_wednesday.containsDate(tuesday),   false);
@@ -30,8 +30,8 @@ describe('iso8601-interval-recurrence', function () {
 			recurrence: 'R/P1W'
 		});
 
-		var wednesday = '2015-07-29T13:00:00.000Z';
-		var tuesday = '2015-07-23T11:12:13.000Z';
+		var wednesday = new Date('2015-07-29T13:00:00.000Z');
+		var tuesday = new Date('2015-07-23T11:12:13.000Z');
 
 		assert.equal(every_wednesday.containsDate(wednesday), true);
 		assert.equal(every_wednesday.containsDate(tuesday),   false);
@@ -44,8 +44,8 @@ describe('iso8601-interval-recurrence', function () {
 			recurrence: 'R2/P1W'
 		});
 
-		var two_weeks_after = '2015-08-05T00:00:00.000Z';
-		var three_weeks_after = '2015-08-12T00:00:00.000Z';
+		var two_weeks_after = new Date('2015-08-05T00:00:00.000Z');
+		var three_weeks_after = new Date('2015-08-12T00:00:00.000Z');
 
 		assert.equal(every_wednesday_for_two_weeks.containsDate(two_weeks_after),   true);
 		assert.equal(every_wednesday_for_two_weeks.containsDate(three_weeks_after), false);
@@ -57,8 +57,8 @@ describe('iso8601-interval-recurrence', function () {
 			interval: '2015-07-22T00:00:00.000Z/2015-07-22T12:00:00.000Z',
 		});
 
-		var before_midday = '2015-07-22T09:00:00.000Z';
-		var after_midday = '2015-07-22T17:00:00.000Z';
+		var before_midday = new Date('2015-07-22T09:00:00.000Z');
+		var after_midday = new Date('2015-07-22T17:00:00.000Z');
 
 		assert.equal(between_midnight_and_midday.containsDate(before_midday), true);
 		assert.equal(between_midnight_and_midday.containsDate(after_midday),  false);
@@ -73,9 +73,9 @@ describe('iso8601-interval-recurrence', function () {
 			recurrence: 'R/P1W'
 		});
 
-		var end_of_a_saturday_in_may = '2015-05-09T23:00:00.000Z';
-		var end_of_a_saturday_in_june = '2015-06-06T23:00:00.000Z';
-		var not_weekend = '2015-06-05T20:00:00.000Z';
+		var end_of_a_saturday_in_may = new Date('2015-05-09T23:00:00.000Z');
+		var end_of_a_saturday_in_june = new Date('2015-06-06T23:00:00.000Z');
+		var not_weekend = new Date('2015-06-05T20:00:00.000Z');
 
 		var weekend_in_may = weekend.currentRange(end_of_a_saturday_in_may);
 		assert.equalTime(weekend_in_may.start, new Date('2015-05-09T00:00:00.000Z'));

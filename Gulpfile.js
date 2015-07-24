6d96c5efad2e51120a14579aba632ebf8ca03f73
Sam Gaus <sam@gaus.co.uk>
2015-07-24 10:31:31 +0100

    Added uglifying and sourcemapping.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,17 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
+var sourcemaps = require("gulp-sourcemaps");
+var uglify = require("gulp-uglify");
 
-gulp.task("babel", function () {
+gulp.task("javascript", function () {
   return gulp.src("src/index.js")
+  	.pipe(sourcemaps.init())
     .pipe(babel())
+    .pipe(uglify())
+    .pipe(sourcemaps.write("."))
     .pipe(gulp.dest("dist"));
 });
 
 gulp.task("watch", function () {
-	gulp.watch("src/**/*.js", ["babel"]);
+	gulp.watch("src/**/*.js", ["javascript"]);
 });

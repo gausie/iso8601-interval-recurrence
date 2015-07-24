aa1aa8a7a763c1a286b36533ff55b89cb9c84fdd
Sam Gaus <sam@gaus.co.uk>
2015-07-24 11:36:30 +0100

    Added tests for currentRange.

--- test/test.js
+++ test/test.js
@@ -1,6 +1,12 @@
-var assert = require('assert');
+// Import and configure assertion framework
+var chai = require('chai');
+chai.use(require('chai-datetime'));
+var assert = chai.assert;
+
+// Import the library to test.
 var IntervalRecurrence = require('../dist/');
 
+// Describe tests.
 describe('iso8601-interval-recurrence', function () {
 
 	it('should be able to check a date against a start date and interval', function () {
@@ -56,6 +62,31 @@ describe('iso8601-interval-recurrence', function () {
 
 		assert.equal(between_midnight_and_midday.containsDate(before_midday), true);
 		assert.equal(between_midnight_and_midday.containsDate(after_midday),  false);
+	});
+
+	it('should be able to return the range that a current date is in', function () {
+		// This is only really useful for repetitions.
+
+		// Matches every weekend.
+		var weekend = new IntervalRecurrence({
+			interval: '2015-W01-6T/P2D',
+			recurrence: 'R/P1W'
+		});
+
+		var end_of_a_saturday_in_may = '2015-05-09T23:00:00.000Z';
+		var end_of_a_saturday_in_june = '2015-06-06T23:00:00.000Z';
+		var not_weekend = '2015-06-05T20:00:00.000Z';
+
+		var weekend_in_may = weekend.currentRange(end_of_a_saturday_in_may);
+		assert.equalTime(weekend_in_may.start, new Date('2015-05-09T00:00:00.000Z'));
+		assert.equalTime(weekend_in_may.end,   new Date('2015-05-11T00:00:00.000Z'));
+
+		var weekend_in_june = weekend.currentRange(end_of_a_saturday_in_june);
+		assert.equalTime(weekend_in_june.start, new Date('2015-06-06T00:00:00.000Z'));
+		assert.equalTime(weekend_in_june.end,   new Date('2015-06-08T00:00:00.000Z'));
+
+		var fails = weekend.currentRange(not_weekend);
+		assert.equal(fails, false);
 	});
 
 });
